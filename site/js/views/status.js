5c941580d599acbbfb92da61e7838b4202f57c14
Will McFarland <will@thenumbercreative.com>
2013-10-19 13:17:08 -0700

    proper stream stop behaviour not pause, fix entypo

--- site/js/views/status.js
+++ site/js/views/status.js
@@ -7,17 +7,23 @@ define(['marionette','hbs!tpl/status'], function(Marionette,tpl) {
     template: tpl,
     events: {
       'click #play-control': 'play',
+      'play': 'debug',
     },
     play: function(e) {
       console.log('play clicked');
       console.log(this);
       if ($('#play-control').hasClass('playing')) { 
-        $('audio')[0].pause();
+        $('audio').prop('src','');
+        $('audio').prop('src',$('audio').attr('data-src'));
         $('#play-control').removeClass('playing').html("&#9654;");
       } else {
         $('audio')[0].play();
-        $('#play-control').addClass('playing').html("&#8214;");
+        $('#play-control').addClass('playing').html("&#9632;");
       }
+    },
+    debug: function(e) {
+      console.log("DEBUG");
+      console.log(e);
     }
   });
 });
