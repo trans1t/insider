9bc6b04f7c49e512b383503c75f827e9af255333
Will McFarland <will@thenumbercreative.com>
2013-10-20 12:09:58 -0700

    adding various minor enhancements to presentation

---
+++ site/js/config-example.js
@@ -0,0 +1,4 @@
+define({
+  //server_address: 'http://insider.fm:3000'
+  server_address: 'http://localhost:3000'
+});
--- site/js/views/status.js
+++ site/js/views/status.js
@@ -15,10 +15,16 @@ define(['marionette','hbs!tpl/status'], function(Marionette,tpl) {
       if ($('#play-control').hasClass('playing')) { 
         $('audio').prop('src','');
         $('audio').prop('src',$('audio').attr('data-src'));
-        $('#play-control').removeClass('playing').html("&#9654;");
+        $('#play-control').removeClass('playing')
+                          .removeClass('btn-danger')
+                          .addClass('btn-success')
+                          .html("&#9654;");
       } else {
         $('audio')[0].play();
-        $('#play-control').addClass('playing').html("&#9632;");
+        $('#play-control').addClass('playing')
+                          .removeClass('btn-success')
+                          .addClass('btn-danger')
+                          .html("&#9632;");
       }
     },
     debug: function(e) {
