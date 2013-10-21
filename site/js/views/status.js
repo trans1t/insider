0d18ee009230ee4b479f3eed5b4489780ea820bb
Will McFarland <will@thenumbercreative.com>
2013-10-20 20:29:46 -0700

    fixing start stop audio stream behaviour, adding reload if error or stream end

--- site/js/views/status.js
+++ site/js/views/status.js
@@ -6,30 +6,49 @@ define(['marionette','hbs!tpl/status'], function(Marionette,tpl) {
     el: '#content',
     template: tpl,
     events: {
-      'click #play-control': 'play',
-      'play': 'debug',
+      'click #play-control': 'playAudio'
     },
-    play: function(e) {
-      console.log('play clicked');
-      console.log(this);
+    playAudio: function(e) {
       if ($('#play-control').hasClass('playing')) { 
-        $('audio').prop('src','');
-        $('audio').prop('src',$('audio').attr('data-src'));
+        $('audio')[0].pause();
+        $('audio').removeAttr('src');
+        $('audio').attr('src',$('audio').attr('data-src'));
         $('#play-control').removeClass('playing')
                           .removeClass('btn-danger')
                           .addClass('btn-success')
                           .html("&#9654;");
       } else {
         $('audio')[0].play();
-        $('#play-control').addClass('playing')
-                          .removeClass('btn-success')
-                          .addClass('btn-danger')
-                          .html("&#9632;");
       }
     },
-    debug: function(e) {
-      console.log("DEBUG");
-      console.log(e);
+    loadingAudio: function() {
+      console.log("LOADING");
+      $('#play-control').removeClass('btn-success')
+                        .addClass('btn-warning')
+                        .html('&#9203;');
+    },
+    playingAudio: function() {
+      console.log("PLAYING");
+      $('#play-control').addClass('playing')
+                        .removeClass('btn-success')
+                        .removeClass('btn-warning')
+                        .addClass('btn-danger')
+                        .html("&#9632;");
+    },
+    error: function() {
+      $('#play-control').removeClass('btn-success')
+                        .removeClass('btn-danger')
+                        .addClass('btn-warning')
+                        .addClass('playing')
+                        .html("&#10226;")
+    },
+    onRender: function() {
+      var self=this;
+      $('audio').on('play',function(e) { self.loadingAudio() });
+      $('audio').on('playing',function(e) { self.playingAudio() });
+      $('audio').on('error',function(e) { self.error() });
+      $('audio').on('ended',function(e) { console.log("ended");self.error() });
+      $('audio').on('progress',function(e) { console.log("progress") });
     }
   });
 });
